296735fa7ab615ed3cf31e08f01b4ef88014417d
Kunaal Bajwa <kunaal.bajwa@gmail.com>
2016-10-09 14:20:24 -0500

    lomo

--- main.py
+++ main.py
@@ -19,8 +19,11 @@ class BlogHandler(webapp2.RequestHandler):
             Get all posts by a specific user, ordered by creation date (descending).
             The user parameter will be a User object.
         """
+        query= Post.all().filter("author", user).order('-created')
+        return query.fetch(limit = limit, offset = offset)
 
-        # TODO - filter the query so that only posts by the given user
+                # LIMIT %d OFFSET %d")
+        # TODO - filter the query so that only posts by the given user;; done
         return None
 
     def get_user_by_name(self, username):
